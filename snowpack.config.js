e7b437319bea3db9dfbed5f39ae179fce3661d71
Horacio Herrera <hi@horacioh.com>
2020-11-01 00:45:25 +0100

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: ['@snowpack/plugin-dotenv', '@prefresh/snowpack'],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+
+  alias: {
+    /* ... */
+  },
+};
